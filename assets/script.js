43207a2e91b0edc5a7afdc07c24e95044b2ae052
CherylHemphill <126039394+CherylHemphill@users.noreply.github.com>
2023-04-25 18:52:20 -0500

    psuedoCode

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,14 @@
-let citySearch = $('#search')
-
+// Activate Search Button
+// Link search button to location API
+// Set searched loctions to local storage
+// Display current forecast
+// Display 5 day forecast
+let URLsearch = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}'
 
 
 $(document).ready(function(){ //defer JQuery until page loads
 
+    $("submit-search").click(function())
 
 
 
